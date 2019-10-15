0b431b014b61d18d17ee1f8468fdd7ba740af56b
Matjaz <matjaz.bohoric@gmail.com>
2019-10-15 12:55:47 +0200

    Added comments for sync/async annotations

--- movies-app/movies-api/src/main/java/com/demo/movies/api/annotation/Asynchronous.java
+++ movies-app/movies-api/src/main/java/com/demo/movies/api/annotation/Asynchronous.java
@@ -1,8 +1,5 @@
 package com.demo.movies.api.annotation;
 
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
@@ -11,8 +8,14 @@ import java.lang.annotation.Target;
 
 import javax.inject.Qualifier;
 
+/**
+ * Annotates asynchronous quality of the resource
+ * 
+ * @author Matjaz
+ *
+ */
 @Retention(RUNTIME)
-@Target({ TYPE, FIELD, METHOD, PARAMETER })
+@Target({ TYPE })
 @Qualifier
 public @interface Asynchronous {
 
