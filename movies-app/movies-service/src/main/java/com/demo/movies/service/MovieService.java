3a4dc88180dc6e7ef711922013e34edb5834e6e5
Matjaz <matjaz.bohoric@gmail.com>
2019-10-15 09:46:21 +0200

    Added service layer, configuration and a simple service class.

---
+++ movies-app/movies-service/src/main/java/com/demo/movies/service/MovieService.java
@@ -0,0 +1,34 @@
+package com.demo.movies.service;
+
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+import com.demo.movies.data.model.Movie;
+
+/**
+ * Business logic provider class.
+ * 
+ * @author Matjaz
+ *
+ */
+@RequestScoped
+public class MovieService {
+
+	@PersistenceContext
+	private EntityManager em;	
+
+	public List<Movie> getAllMovies() {
+		TypedQuery<Movie> query = em.createNamedQuery("Movie.fetchAll", Movie.class);
+		List<Movie> result = query.getResultList();
+		return result;
+	}
+
+	public void newMovie(Movie movie) {
+		em.persist(movie);
+	}	
+
+}
