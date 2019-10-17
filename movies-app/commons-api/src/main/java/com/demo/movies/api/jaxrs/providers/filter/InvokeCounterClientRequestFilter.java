c945bedfd4df3929a4bfcb7a40d41422d4b1c130
Matjaz <matjaz.bohoric@gmail.com>
2019-10-17 14:02:25 +0200

    Better identification of REST methods that are being called.

--- movies-app/commons-api/src/main/java/com/demo/movies/api/jaxrs/providers/filter/InvokeCounterClientRequestFilter.java
+++ movies-app/commons-api/src/main/java/com/demo/movies/api/jaxrs/providers/filter/InvokeCounterClientRequestFilter.java
@@ -2,15 +2,27 @@ package com.demo.movies.api.jaxrs.providers.filter;
 
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
 import java.util.Objects;
 
 import javax.inject.Inject;
 import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerRequestFilter;
+import javax.ws.rs.core.PathSegment;
+import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.ext.Provider;
 
 import com.demo.movies.api.counter.service.CounterService;
 
+/**
+ * Inspects the request and identifies the current REST method that is being called.
+ * Then increases the counter which represents how many times a ceratin REST method
+ * was invoked.
+ * 
+ * @author Matjaz
+ *
+ */
 @Provider
 public class InvokeCounterClientRequestFilter implements ContainerRequestFilter  {
 
@@ -21,14 +33,24 @@ public class InvokeCounterClientRequestFilter implements ContainerRequestFilter 
 	public void filter(ContainerRequestContext requestContext) throws IOException {
 		Objects.nonNull(requestContext);
 		
-		String path = requestContext.getUriInfo().getPath();
-		String method = requestContext.getMethod();
+		UriInfo uriInfo = requestContext.getUriInfo();
+
+		String pathId = "";	// string constructed from path segments that are not parameters, used to identify counter
 		
-		if (path!=null) {
-			path = path.replaceAll("/", "");
-			if (method==null) method = "NOMETHOD";
-			counterService.increaseCounter(path + "_" + method);		
+		// scan path segments and use only those segments (for constructing counterId) that are not parameters
+		Collection<List<String>> paramValues = uriInfo.getPathParameters().values();
+		for (PathSegment pathSegment : uriInfo.getPathSegments()) {
+			String path = pathSegment.getPath();
+			boolean pathSegementIsParameter = paramValues.stream().anyMatch(list -> {
+				return list.contains(path);
+			});
+			if (!pathSegementIsParameter) pathId += "_" + path; 
 		}
+		
+		String method = requestContext.getMethod();
+		if (method==null) method = "NOMETHOD";
+		//counterService.increaseCounter(method + "_" + pathId);		
+		counterService.increaseCounter(String.format("%s_%s", method, pathId));
 	}
 
 }
