a170000f911283fa0f4f908c5e03c597226575d8
Matjaz <matjaz.bohoric@gmail.com>
2019-10-19 12:59:36 +0200

    Dates serialization

---
+++ movies-app/commons-api/src/main/java/com/demo/movies/api/jaxrs/providers/serialization/ObjectMapperContextResolver.java
@@ -0,0 +1,32 @@
+package com.demo.movies.api.jaxrs.providers.serialization;
+
+import java.text.SimpleDateFormat;
+import java.util.TimeZone;
+
+import javax.ws.rs.ext.ContextResolver;
+import javax.ws.rs.ext.Provider;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+@Provider
+public class ObjectMapperContextResolver implements ContextResolver<ObjectMapper> {
+
+    final ObjectMapper defaultMapper = new ObjectMapper();
+
+    
+    public ObjectMapperContextResolver() {
+        defaultMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);	// disable serialization to numeric timestamp
+        
+        // set format for serializing date(time)s
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssXXX");
+        dateFormat.setTimeZone(TimeZone.getTimeZone("UTC"));
+        defaultMapper.setDateFormat(dateFormat);
+    }
+
+    @Override
+    public ObjectMapper getContext(Class<?> type) {
+        return defaultMapper;
+    }  
+}
+
--- movies-app/movies-data/src/main/resources/sql/seed.sql
+++ movies-app/movies-data/src/main/resources/sql/seed.sql
@@ -2,9 +2,11 @@ insert into movie(id, title, year, description) values ('tt0083658', 'Blade Runn
 insert into movie(id, title, year, description) values ('tt1856101', 'Blade Runner 2049', 2017, 'A young blade runner`s discovery of a long-buried secret leads him to track down former blade runner Rick Deckard, who`s been missing for thirty years.')
 insert into movie(id, title, year, description) values ('tt0163651', 'American Pie', 1999, 'Four teenage boys enter a pact to lose their virginity by prom night.')
 
-insert into actor(id, firstName, lastName) values (10000, 'Harrison', 'Ford')
-insert into actor(id, firstName, lastName) values (10001, 'Ryan', 'Gosling')
+insert into actor(id, firstName, lastName, dateOfBirth) values (10000, 'Harrison', 'Ford', '1942-07-13')
+insert into actor(id, firstName, lastName, dateOfBirth) values (10001, 'Ryan', 'Gosling', '1980-11-12')
+insert into actor(id, firstName, lastName, dateOfBirth) values (10002, 'Seann', 'William Scott', '1976-10-03')
 
 insert into movies_actors (movie_id, actor_id) values ('tt0083658', 10000)
 insert into movies_actors (movie_id, actor_id) values ('tt1856101', 10000)
 insert into movies_actors (movie_id, actor_id) values ('tt1856101', 10001)
+insert into movies_actors (movie_id, actor_id) values ('tt0163651', 10002)
